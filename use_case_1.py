4f39a610c3bd13f3097cc625a14014069ef07375
Oleksandr Kaleniuk <akalenuk@gmail.com>
2022-12-31 17:50:11 +0200

    Update use_case_1.py

--- use_case_1.py
+++ use_case_1.py
@@ -15,50 +15,50 @@ EPS = 0.0001
 ITERS = 10000
 
 def min1(f, iters = ITERS):
-    x = 0
-    for i in range(iters):
-        e = EPS
-        if f(x) <= f(x-e) and f(x) <= f(x+e):
-            return x
-        if f(x-e) < f(x+e):
-            e = -e
-        while f(x+2*e) < f(x+e):
-            e*=2
-        x = x+e
-    raise Exception('min', 'Local minimum not found in ' + str(iters) + ' iterations')
+	x = 0
+	for i in range(iters):
+		e = EPS
+		if f(x) <= f(x-e) and f(x) <= f(x+e):
+			return x
+		if f(x-e) < f(x+e):
+			e = -e
+		while f(x+2*e) < f(x+e):
+			e*=2
+		x = x+e
+	raise Exception('min', 'Local minimum not found in ' + str(iters) + ' iterations')
 
-    
+
 def minn(f, n, iters = ITERS):
-    xi = [0]*n
+	xi = [0]*n
 
-    def fi(i):
-        return lambda x: f(*([xi[j] for j in range(i)] + [x] + [xi[j] for j in range(i+1, n)])) 
-    
-    for i in range(iters):
-        oxi = [x for x in xi]
-        for j in range(n):
-            xi[j] = min1( fi(j) )
-        if sum([(xi[j]-oxi[j])**2 for j in range(n)]) == 0:
-            return xi
+	def fi(i):
+		return lambda x: f(*([xi[j] for j in range(i)] + [x] + [xi[j] for j in range(i+1, n)])) 
+	
+	for i in range(iters):
+		oxi = [x for x in xi]
+		for j in range(n):
+			xi[j] = min1( fi(j) )
+		if sum([(xi[j]-oxi[j])**2 for j in range(n)]) == 0:
+			return xi
 
-    raise Exception('minn', 'Local minimum not found in ' + str(iters) + ' iterations') 
-    
+	raise Exception('minn', 'Local minimum not found in ' + str(iters) + ' iterations') 
+
 
 def Rn_to_01(a):
-    return 0.5 * (1 + math.atan(a) * 2 / math.pi)
+	return 0.5 * (1 + math.atan(a) * 2 / math.pi)
 
 
 def min_mans_n(f, mans, n):
-    xs = []
-    for man in mans:
-        def onRn(*x):
-            on_man = man(*[Rn_to_01(xi) for xi in x])
-            return f(*on_man)
-        x = minn(onRn, n)
-        rx = man(*[Rn_to_01(xi) for xi in x])
-        fx = f(*rx)
-        xs += [(fx, rx)]
-    return min(xs)[1]
+	xs = []
+	for man in mans:
+		def onRn(*x):
+			on_man = man(*[Rn_to_01(xi) for xi in x])
+			return f(*on_man)
+		x = minn(onRn, n)
+		rx = man(*[Rn_to_01(xi) for xi in x])
+		fx = f(*rx)
+		xs += [(fx, rx)]
+	return min(xs)[1]
 
 
 if __name__ == "__main__":
@@ -71,11 +71,9 @@ if __name__ == "__main__":
 	def manifold2_2(x, y):
 		return (1.0 + 2*x - math.cos(2*y), 1 + 2*math.cos(x) + y*2)
 
- 
 	# graphics
 	from html_bitmap import *
 	bmp = new_bitmap(500, 500)
-	
 
 	for i in range(0, 500, 1):
 		for j in range(0, 500, 1):
@@ -95,15 +93,11 @@ if __name__ == "__main__":
 			rx = int(xy[0]*100)
 			ry = int(xy[1]*100)
 			pixel_on(bmp, rx, ry, "#0ee")
-     
-    
+
 	x = min_mans_n(f2, [manifold2_1, manifold2_2], 2)
 
 	line_on(bmp, x[0]*100-5, x[1]*100, x[0]*100, x[1]*100, "#ee0", 2)
 	line_on(bmp, x[0]*100, x[1]*100+5, x[0]*100, x[1]*100, "#ee0", 2)
 	line_on(bmp, x[0]*100-15, x[1]*100+15, x[0]*100, x[1]*100, "#ee0", 2)
 
-    
-	print to_html(bmp) 
-
-	
+	print( to_html(bmp) )
